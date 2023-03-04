3266280ccb5933c8fb3297ce1d3249d7bb43d090
JonasKucinskas <jonaskucinskas2@gmail.com>
2023-03-04 18:29:23 +0200

    MM-2 <updated UI, activity now uses fragments>

--- app/src/main/java/com/example/milemate/CheckUpPage.kt
+++ app/src/main/java/com/example/milemate/CheckUpPage.kt
@@ -5,39 +5,18 @@ import android.os.Bundle
 import android.view.View
 import android.widget.DatePicker
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
 
 
 class CheckUpPage : AppCompatActivity() {
 
     private lateinit var datePicker : DatePicker
-    private lateinit var calendar : Calendar
+    private lateinit var calendar: Calendar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_up_page)
 
-        calendar = Calendar.getInstance()
-        datePicker = findViewById(R.id.setCheckUpDatePicker)//get datePicker object
-
-
-
-
-    }
-
-    fun openDatePicker(view: View) {
-
-        val dateSetListener = DatePickerDialog.OnDateSetListener { view, year, monthOfYear, dayOfMonth ->
-            calendar.set(Calendar.YEAR, year)
-            calendar.set(Calendar.MONTH, monthOfYear)
-            calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
-
-            //update header of datepicker
-            datePicker.updateDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
-        }
-
-        //Open date picker dialog
-        DatePickerDialog(this@CheckUpPage, dateSetListener,
-            calendar.get(Calendar.YEAR),
-            calendar.get(Calendar.MONTH),
-            calendar.get(Calendar.DAY_OF_MONTH)).show()
     }
 }
