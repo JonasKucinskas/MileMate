90aced921a3ec2bd2ce13a561e6c2e67d4f0aef3
JonasKucinskas <jonaskucinskas2@gmail.com>
2023-03-05 17:04:22 +0200

    MM-2 fixed fragment layouts, added navigation button and SetReminderFragment

--- app/src/main/java/com/example/milemate/CheckUpPage.kt
+++ app/src/main/java/com/example/milemate/CheckUpPage.kt
@@ -1,9 +1,7 @@
 package com.example.milemate
-import android.icu.util.Calendar
-import android.os.Build
 import android.os.Bundle
-import android.widget.DatePicker
-import androidx.annotation.RequiresApi
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import androidx.fragment.app.replace
@@ -11,13 +9,18 @@ import androidx.fragment.app.replace
 
 class CheckUpPage : AppCompatActivity() {
 
-    private lateinit var datePicker : DatePicker
-    private lateinit var calendar: Calendar
-
-    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_check_up_page)
+
+    }
+    fun on_Click_NextButton(view: View) {//switch to set reminder fragment
+
+        supportFragmentManager.commit {
+            replace<CheckUpReminderFragment>(R.id.fragmentContainerView)
+            setReorderingAllowed(true)
+            addToBackStack("name")
+        }
 
     }
 }
