492a47e9a75256a4d0f0974b6702641dbc118814
JonasKucinskas <jonaskucinskas2@gmail.com>
2023-03-10 21:57:06 +0200

    MM-2 added dynamic numPickers

--- app/src/main/java/com/example/milemate/CheckUpFragment.kt
+++ app/src/main/java/com/example/milemate/CheckUpFragment.kt
@@ -1,7 +1,5 @@
 package com.example.milemate
 
-import android.annotation.SuppressLint
-import android.icu.text.SimpleDateFormat
 import android.icu.util.Calendar
 import android.os.Build
 import android.os.Bundle
@@ -14,6 +12,7 @@ import android.widget.NumberPicker
 import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
+import java.text.DateFormat.getDateTimeInstance
 import java.util.*
 
 
@@ -22,11 +21,7 @@ import java.util.*
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [CheckUpPage_DatePicker_Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class CheckUpFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
@@ -38,21 +33,12 @@ class CheckUpFragment : Fragment() {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-
-
-
-
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-
-
-
-
         return inflater.inflate(R.layout.fragment_check_up, container, false)
     }
 
@@ -76,7 +62,6 @@ class CheckUpFragment : Fragment() {
             }
     }
 
-    @SuppressLint("SimpleDateFormat")
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -85,39 +70,53 @@ class CheckUpFragment : Fragment() {
         val numPickerMonth = view.findViewById<NumberPicker>(R.id.numberPickerMonth)
         val numPickerDay = view.findViewById<NumberPicker>(R.id.numberPickerDay)
 
-        numPickerYear.minValue = 0
-        numPickerYear.maxValue = 4
 
-        numPickerMonth.minValue = 0
-        numPickerMonth.maxValue = 12
-
-        numPickerDay.minValue = 1
-        numPickerDay.maxValue = 31
 
         val checkUpSaveBtn = view.findViewById<Button>(R.id.CheckUpSaveBtn)
         val datePicker = view.findViewById<DatePicker>(R.id.CheckUpDatePicker)
 
         val calendar = Calendar.getInstance()
-        datePicker.minDate = calendar.timeInMillis//set min available date as today in datePicker
+        datePicker.minDate = calendar.timeInMillis+24*60*60*1000//set min available date to tomorrow in datePicker
 
-        checkUpSaveBtn.setOnClickListener(){
+        val calendarSet = Calendar.getInstance()
 
-            calendar.set(datePicker.year, datePicker.month, datePicker.dayOfMonth)
+        //dynamic adjustment of numPickers. This ensures that negative date can't be set for reminder.
+        datePicker.setOnDateChangedListener { datePicker, year, month, day ->
 
-            //need to convert to negative values for subtraction.
-            val years =  -numPickerYear.value
-            val months = -numPickerMonth.value
-            val days = -numPickerDay.value
+            calendarSet.set(year, month+1, day)//+1, because months start at 1
+
+            //subtract current date from selected date to get time difference.
+            calendarSet.add(Calendar.YEAR, -Calendar.getInstance().get(Calendar.YEAR))
+            calendarSet.add(Calendar.MONTH, -Calendar.getInstance().get(Calendar.MONTH)-1)//-1 because months start at 0
+            calendarSet.add(Calendar.DAY_OF_MONTH, -Calendar.getInstance().get(Calendar.DAY_OF_MONTH))
+
+            numPickerYear.minValue = 0
+            numPickerYear.maxValue = calendarSet.get(Calendar.YEAR)//cant set year as 0 (?), so -1 'fixes' it
+
+            numPickerMonth.minValue = 0
+            numPickerMonth.maxValue = calendarSet.get(Calendar.MONTH)
+
+            numPickerDay.minValue = 0
+            numPickerDay.maxValue = calendarSet.get(Calendar.DAY_OF_MONTH)
+
+            //TODO Fix numPickers. This is shit implementation. if check-up date is exactly year later, user has to select year in numPickerYear, numPickerMonth will have max num as 0, even tho it should have it as 12.
+        }
+
+        checkUpSaveBtn.setOnClickListener{
+
+            calendar.set(datePicker.year, datePicker.month, datePicker.dayOfMonth)//set calendar object to selected date in datePicker
 
             //subtraction from selected date in date picker
-            calendar.add(Calendar.YEAR, years)
-            calendar.add(Calendar.MONTH, months)
-            calendar.add(Calendar.DAY_OF_MONTH, days)
+            calendar.add(Calendar.YEAR, -numPickerYear.value)//Current year - numpicker.Year
+            calendar.add(Calendar.MONTH, -numPickerMonth.value)
+            calendar.add(Calendar.DAY_OF_MONTH, -numPickerDay.value)
 
-            val dateFormat = SimpleDateFormat("yyyy-MM-dd")
+
+            val dateFormat = getDateTimeInstance()
             val reminderDate = dateFormat.format(calendar.time)
 
             Toast.makeText(activity, "Reminder set for $reminderDate", Toast.LENGTH_SHORT).show()
         }
+
     }
 }
