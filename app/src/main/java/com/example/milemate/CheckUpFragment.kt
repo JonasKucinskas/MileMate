5269630a6f0ee5f0e9dbf21b87894452b7967057
JonasKucinskas <jonaskucinskas2@gmail.com>
2023-03-23 23:51:15 +0200

    code 'cleanups'

--- app/src/main/java/com/example/milemate/CheckUpFragment.kt
+++ app/src/main/java/com/example/milemate/CheckUpFragment.kt
@@ -1,7 +1,6 @@
 package com.example.milemate
 
 import android.icu.util.Calendar
-import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +9,6 @@ import android.widget.Button
 import android.widget.DatePicker
 import android.widget.NumberPicker
 import android.widget.Toast
-import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.google.gson.Gson
 import java.io.BufferedWriter
@@ -18,6 +16,7 @@ import java.io.File
 import java.io.FileWriter
 import java.util.concurrent.TimeUnit
 import kotlin.math.ceil
+import kotlin.math.floor
 
 
 // TODO: Rename parameter arguments, choose names that match
@@ -69,29 +68,47 @@ class CheckUpFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        val numPickerYear = view.findViewById<NumberPicker>(R.id.numberPickerYear)
         val numPickerMonth = view.findViewById<NumberPicker>(R.id.numberPickerMonth)
         val numPickerDay = view.findViewById<NumberPicker>(R.id.numberPickerDay)
 
         val checkUpSaveBtn = view.findViewById<Button>(R.id.CheckUpSaveBtn)
         val datePicker = view.findViewById<DatePicker>(R.id.CheckUpDatePicker)
 
+        numberPickersInit(datePicker, numPickerMonth, numPickerDay)
+        updateNumPickerMonthRange(numPickerMonth, numPickerDay)
+        updateNumPickerDayRange(numPickerMonth, numPickerDay)
+
+        checkUpSaveBtn.setOnClickListener{
+
+            val reminderDate = Calendar.getInstance()
+            val checkUpDate = Calendar.getInstance()
+
+            //birką dėjau ant datų
+            //subtract time gap in years from current year
+            var years = datePicker.year - floor((numPickerMonth.value * 31 + numPickerDay.value) / 365.0)
+
+            reminderDate.set(years.toInt(), datePicker.month - numPickerMonth.value, datePicker.dayOfMonth - numPickerDay.value)
+            checkUpDate.set(datePicker.year, datePicker.month, datePicker.dayOfMonth)
+
+            val reminder = Reminder(reminderDate, checkUpDate)
+            val reminderJson = Gson().toJson(reminder)
+
+            writeToJson(reminderJson, "reminder.json")
+
+            Toast.makeText(activity, "Reminder set for ${reminderDate.time}", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun numberPickersInit(datePicker: DatePicker, numPickerMonth: NumberPicker, numPickerDay: NumberPicker){
+
         val calendar = Calendar.getInstance()
         // set calendar time to midnight otherwise days don't round properly
         calendar.set(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH), 0, 0, 0)
 
         datePicker.minDate = calendar.timeInMillis+24*60*60*1000//set min available date to tomorrow in datePicker
-
         //dynamic adjustment of numPickers. This ensures that negative date can't be set for reminder.
         datePicker.setOnDateChangedListener { datePicker, year, month, day ->
 
-            //subtract current date from selected date to get time difference.
-            /*
-            val yearDiff = year - calendar.get(Calendar.YEAR)
-            val monthDiff = month - calendar.get(Calendar.MONTH)
-            var dayDiff = day - calendar.get(Calendar.DAY_OF_MONTH)
-            */
-            //set calendar date to picked date in datepicker
             val calendar2 = Calendar.getInstance()
             calendar2.set(year, month, day)
 
@@ -100,21 +117,19 @@ class CheckUpFragment : Fragment() {
             val dayDiff = TimeUnit.MILLISECONDS.toDays(msDiff)
 
             //not very accurate, but it will work for now.
-            val yearDiff = dayDiff / 365
             val monthDiff = dayDiff / 31
 
             //set numPickers to maximum date for reminder
-            numPickerYear.minValue = 0
-            numPickerYear.maxValue = yearDiff.toInt()
 
             numPickerMonth.minValue = 0
             numPickerMonth.maxValue = monthDiff.toInt()
 
             numPickerDay.minValue = 0
             numPickerDay.maxValue = dayDiff.toInt()
-
         }
+    }
 
+    private fun updateNumPickerMonthRange(numPickerMonth: NumberPicker, numPickerDay: NumberPicker){
         numPickerDay.setOnValueChangedListener { numberPicker, changedFromNum, changedToNum ->
 
             if (changedToNum / 31.0 > 0 && numPickerMonth.maxValue > 0){
@@ -124,7 +139,9 @@ class CheckUpFragment : Fragment() {
                 numPickerMonth.maxValue++
             }
         }
+    }
 
+    private fun updateNumPickerDayRange(numPickerMonth: NumberPicker, numPickerDay: NumberPicker){
         numPickerMonth.setOnValueChangedListener { numberPicker, changedFromNum, changedToNum ->
 
             if (changedToNum > 0){
@@ -135,25 +152,6 @@ class CheckUpFragment : Fragment() {
                 numPickerDay.maxValue += changedFromNum * 31
             }
 
-        }
-
-        checkUpSaveBtn.setOnClickListener{
-
-            //val dateFormat = getDateTimeInstance()
-            //val reminderDate = dateFormat.format(calendar.time)
-
-            val reminderDate = Calendar.getInstance()
-            val checkUpDate = Calendar.getInstance()
-
-            reminderDate.set(datePicker.year - numPickerYear.value, datePicker.month - numPickerMonth.value, datePicker.dayOfMonth - numPickerDay.value)
-            checkUpDate.set(datePicker.year, datePicker.month, datePicker.dayOfMonth)
-
-            val reminder = Reminder(reminderDate, checkUpDate)
-            val reminderJson = Gson().toJson(reminder)
-
-            writeToJson(reminderJson, "reminder.json")
-
-            Toast.makeText(activity, "Reminder set for ${reminderDate.time}", Toast.LENGTH_SHORT).show()
         }
     }
 
